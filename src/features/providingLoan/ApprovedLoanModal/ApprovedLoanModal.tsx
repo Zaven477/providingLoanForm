ac47d5bfa4101fa46181bcc8f593a4304c660478
Zaven477 <Papikyan86@bk.ru>
2026-01-01 16:03:13 +0300

    add changes

--- src/features/providingLoan/ApprovedLoanModal/ApprovedLoanModal.tsx
+++ src/features/providingLoan/ApprovedLoanModal/ApprovedLoanModal.tsx
@@ -17,11 +17,11 @@ export const ApprovedLoanModal = ({
     <>
       {isOpenModal && (
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-[rgba(0.5,0.5,0.5,0.5)] flex justify-center items-center">
-          <div className="bg-[white] h-[110px] w-[430px] flex justify-center">
+          <div className="bg-[white] min-h-[110px] max-w-[430px] flex justify-center ml-2 mr-2">
             <div className="flex flex-col">
-              <p className="mt-4">{message}</p>
+              <p className="mt-4 pl-2 pr-2">{message}</p>
               <button
-                className="h-[30px] mt-6 w-[100px] bg-amber-400 cursor-pointer ml-auto"
+                className="h-[30px] mt-6 mb-2 w-[100px] bg-amber-400 cursor-pointer ml-auto mr-2"
                 onClick={closeModal}
               >
                 Закрыть
